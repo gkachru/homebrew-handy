e05d76e129c0751eaa6a2ff4a9870dfc2e4922c1
Gautam Kachru <gautam@M4Mini.local>
2026-03-28 00:41:05 +0530

    Bump to 0.8.1-fork.4: system audio required, remove banner, paste defaults to none

--- Casks/handy-fork.rb
+++ Casks/handy-fork.rb
@@ -1,6 +1,6 @@
 cask "handy-fork" do
-  version "0.8.1-fork.3"
-  sha256 "654a1e033d8ba7cf39fd06410331bdc69ca709115b288b0a1d9840774ed737a1"
+  version "0.8.1-fork.4"
+  sha256 "b2289519ab596336e4b327a9c8119baf7ad84208ba71db01e8e3f5ef555bcf24"
 
   url "https://github.com/gkachru/Handy/releases/download/v0.8.1-fork/Handy_0.8.1_aarch64.dmg",
       verified: "github.com/gkachru/Handy/"
