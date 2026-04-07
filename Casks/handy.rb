316ffa145af48b41e5e145c878be11c4185f1c93
Gautam Kachru <gautam@M4Mini.local>
2026-04-07 19:09:22 +0530

    handy 0.8.2

---
+++ Casks/handy.rb
@@ -0,0 +1,11 @@
+cask "handy" do
+  version "0.8.2"
+  sha256 "e137d87e70386ddddbb9cc255d9864cd0653bf7bb8dd4d6ed1c3865b466d5f4f"
+
+  url "https://github.com/gkachru/Handy/releases/download/v0.8.2/Handy_0.8.2_aarch64.dmg"
+  name "Handy"
+  desc "Speech-to-text desktop app"
+  homepage "https://github.com/gkachru/Handy"
+
+  app "Handy.app"
+end
