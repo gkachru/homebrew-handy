10ff306c28ff9522523de7907104a84d667095bf
Gautam Kachru <gautam@M4Mini.local>
2026-04-07 19:13:43 +0530

    handy 0.8.2

--- Casks/handy-fork.rb
+++ Casks/handy-fork.rb
@@ -1,28 +1,11 @@
 cask "handy-fork" do
-  version "0.8.1-fork.4"
-  sha256 "b2289519ab596336e4b327a9c8119baf7ad84208ba71db01e8e3f5ef555bcf24"
+  version "0.8.2"
+  sha256 "e137d87e70386ddddbb9cc255d9864cd0653bf7bb8dd4d6ed1c3865b466d5f4f"
 
-  url "https://github.com/gkachru/Handy/releases/download/v0.8.1-fork/Handy_0.8.1_aarch64.dmg",
-      verified: "github.com/gkachru/Handy/"
+  url "https://github.com/gkachru/Handy/releases/download/v0.8.2/Handy_0.8.2_aarch64.dmg"
   name "Handy"
-  desc "Speech to text application (fork with system audio & Mistral streaming)"
+  desc "Speech-to-text desktop app"
   homepage "https://github.com/gkachru/Handy"
 
-  depends_on arch: :arm64
-  depends_on macos: ">= :ventura"
-
   app "Handy.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/Handy.app"],
-                   sudo: false
-  end
-
-  zap trash: [
-    "~/Library/Application Support/com.pais.handy",
-    "~/Library/Caches/com.pais.handy",
-    "~/Library/LaunchAgents/Handy.plist",
-    "~/Library/WebKit/com.pais.handy",
-  ]
 end
